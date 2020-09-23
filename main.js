3e9727b109da01071c421152229060093ebf5c36
deep-coder17 <71636092+deep-coder17@users.noreply.github.com>
2020-09-23 14:45:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.position(480,200);
+}
+function draw(){
+
+}
+function take_snapshot(){
+    save('your_mustached_photo.png');
+}
